f699c7d581d6a3124bd2f6f5cdac49970136e7e3
Bilal Shaikh <bilal101shaikh@gmail.com>
2023-09-23 22:54:56 +0530

    Basic  node Project Template

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+const PORT = 3000;
+
+const apiRoutes = require('./routes')
+// const { PORT } = require('./config/index.js')
+const { Logger } = require('./config')
+
+app.use('/api', apiRoutes)
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+    // Logger.info("Successfully started the server", { error: "Error Occured" })
+})
