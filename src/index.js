1006968f1ae623fe723995d506e5427b22155e33
Bilal Shaikh <bilal101shaikh@gmail.com>
2023-12-30 19:09:54 +0530

    Added basic sequelize and cli related files

--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ app.use('/api', apiRoutes)
 
 app.listen(ServerConfig.PORT, () => {
     console.log("listening on port", ServerConfig.PORT);
-    Logger.info(`Successfully Started the Server ${ServerConfig.PORT}`, { error: "Error Starting the server" })
+    // Logger.info(`Successfully Started the Server ${ServerConfig.PORT}`, { error: "Error Starting the server" })
 })
