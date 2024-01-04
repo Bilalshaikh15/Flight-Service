8316ce429ec8c481776a122837325479cf638c03
Bilal Shaikh <bilal101shaikh@gmail.com>
2024-01-05 00:23:55 +0530

    Added Middleware for createcity Api

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const { CityControllers } = require('../../controllers')
+const { CityMiddlewares } = require('../../middlewares')
 
 const router = express.Router()
 
 router.post('/',
+    CityMiddlewares.validateCreateRequest,
     CityControllers.createCity)
 
 module.exports = router
